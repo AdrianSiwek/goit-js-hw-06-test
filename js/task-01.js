71f0495cfaecf546eb28a9778865401f00453752
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-11-28 21:56:46 +0100

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categories = document.querySelector('#categories');
+const numberOfCategories = categories.querySelectorAll('li.item');
+console.log(`Number of categories: ${numberOfCategories.length}`);
+
+const arrayCategories = numberOfCategories.forEach((item) => {
+    console.log("");
+    console.log(`Category: ${item.querySelector('h2').textContent}`);
+    console.log(`Elements: ${item.querySelector("li").length}`);
+});
